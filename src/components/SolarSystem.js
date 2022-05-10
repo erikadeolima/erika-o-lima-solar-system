b788c3d6dc7d3667619014721199b9127e4016d1
Erika de Oliveira Lima <erikadeo.lima@hotmail.com>
2022-05-10 14:03:21 -0300

    Requisito 6: Concluido

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planets.map((planet) => (<PlanetCard
+          key={ planet.name }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />))}
       </div>
     );
   }
 }
+
+SolarSystem.propTypes = {
+  planets: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }),
+};
+
+SolarSystem.defaultProps = {
+  planets: [],
+};
 
 export default SolarSystem;
