b1898786be43ba4d8b8af10739de906d9cb53469
Erika de Oliveira Lima <erikadeo.lima@hotmail.com>
2022-05-10 14:09:55 -0300

    Requisito 7: Concluido

---
+++ src/components/Missions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+/* import PropTypes from 'prop-types';
+ */
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions" />
+    );
+  }
+}
+
+Missions.propTypes = {
+};
+
+export default Missions;
